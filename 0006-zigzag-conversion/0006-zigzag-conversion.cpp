7f6d616a1b131522316ed3d991f789ad82c0633f
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-02-13 18:38:37 +0600

    Time: 24 ms (22.95%), Space: 16.5 MB (11.53%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        
+       if (numRows == 1)
+        return s;
+
+    vector<vector<char>> ans(numRows);
+
+    int index = 0;
+    int direction = 1; // 1 for down, -1 for up
+
+    for (char c : s) {
+        ans[index].push_back(c);
+        if (index == 0)
+            direction = 1;
+        else if (index == numRows - 1)
+            direction = -1;
+        index += direction;
+    }
+
+    string result;
+    for (const auto& row : ans) {
+        for (char c : row) {
+            result.push_back(c);
+        }
+    }
+
+    return result;
+    }
+};
