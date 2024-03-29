52f8bbcb020d8224059b3ab2edf4a14ad35ed724
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-03-29 12:51:52 +0600

    Time: 126 ms (74.61%), Space: 120.1 MB (51.36%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -4,18 +4,19 @@ public:
         int maxi=*max_element(nums.begin(),nums.end());
         long long  ans=0;long long  left=0;long long  right=0;
         unordered_map<int,int>mp;
+        int count=0;
         while(right<nums.size()){
-           mp[nums[right]]++;
-           while(left<=right&&mp[maxi]>=k){
-              ans += nums.size() - right;
-            mp[nums[left]]--;
-            if (mp[nums[left]] == 0)
-                mp.erase(nums[left]);
-            left++;
+           if(nums[right]==maxi){
+               count++;
+           }
+           while(left<=right&&count>=k){
+               ans += nums.size()-right;
+               if(nums[left]==maxi){
+                   count--;
+               }
+               left++;
            }
             right++;
-            
-            
         }
         return ans;
     }
