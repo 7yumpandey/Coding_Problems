6ca10d7acdf0955444ccc0b1ec82e5fe6c7f279d
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-04-15 00:11:03 +0600

    Time: 0 ms (100.00%), Space: 14.8 MB (48.75%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -11,20 +11,20 @@
  */
 class Solution {
 public:
-    void ans1(TreeNode *root,int &sum){
-        if(root==NULL){
-            return;
-        }
-        if(root->left!=NULL&&root->left->right==NULL&&root->left->left==NULL){
-            sum=sum+root->left->val;
-        };
-        ans1(root->left,sum);
-        ans1(root->right,sum);
-    }
     int sumOfLeftLeaves(TreeNode* root) {
-        int ans=0;
-        ans1(root,ans);
-        return ans;
-
+        return dfs(root, false);
     }
+    
+    int dfs(TreeNode* node, bool isLeft) {
+        if (!node) {
+            return 0;
+        }
+        if (!node->left && !node->right) { // Leaf node
+            return isLeft ? node->val : 0;
+        }
+        int leftSum = dfs(node->left, true);    // Traverse left child
+        int rightSum = dfs(node->right, false); // Traverse right child
+        return leftSum + rightSum;
+    }
+
 };
