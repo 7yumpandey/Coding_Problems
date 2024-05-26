425d79b9ae503c327a79073c56db90ef23347edf
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-05-27 00:10:54 +0600

    Time: 0 ms (100.00%), Space: 23.1 MB (25.75%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,31 +2,57 @@ class Solution {
 public:
 
     string reverseWords(string s) {
-        stack<string>st;
-        string str="";
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' '){
-                if(str!=""){
-                st.push(str);
-                str="";
+        // stack<string>st;
+        // string str="";
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]==' '){
+        //         if(str!=""){
+        //         st.push(str);
+        //         str="";
+        //         }
+        //     }
+        //     else{
+        //         str+=s[i];
+        //     }
+        // }
+        // if(str!=""){
+        //     st.push(str);
+        // }
+        // string ans="";
+        // while(!st.empty()){
+        //      string a=st.top();
+        //      st.pop();
+        //      ans += a;
+        //      if (!st.empty()) {
+        //          ans +=" ";
+        //      }
+        // }
+        // return ans;
+        
+        int n=s.size();
+        int left=0;
+        string ans="",temp="";
+        while(left<n){
+            char ch= s[left];
+        if (ch != ' ') {
+            temp += ch;
+        } else if (ch == ' ') {if (temp != "") {
+                if (ans != "") {
+                    ans = temp + " " + ans;
+                } else {
+                    ans = temp;
                 }
-            }
-            else{
-                str+=s[i];
+                temp = "";
             }
         }
-        if(str!=""){
-            st.push(str);
+        left++;
         }
-        string ans="";
-        while(!st.empty()){
-             string a=st.top();
-             st.pop();
-             ans += a;
-             if (!st.empty()) {
-                 ans +=" ";
-             }
-        }
+        
+        if (temp!="") {
+        if (ans!=""&&temp!="") ans = temp + " " + ans;
+        else ans = temp;
+    }
         return ans;
+        
     }
 };
