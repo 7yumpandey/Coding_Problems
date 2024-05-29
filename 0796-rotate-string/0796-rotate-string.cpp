29de7be21e7e9488dbdcecb3b0c775bcf45cce3a
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-05-30 00:56:53 +0600

    Time: 3 ms (48.18%), Space: 9.8 MB (5.81%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,10 +1,30 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-     if (s.size() != goal.size()) {
+        queue<int>q1,q2;
+        int n=s.size(),m=goal.size();
+        if(n!=m){
+            return false;
+        }
+        if(s==goal){
+            return true;
+        }
+        for(int i=0;i<n;i++){
+            q1.push(s[i]);
+            q2.push(goal[i]);
+        }
+        int i=0;
+        while(i<=n){
+            if(q1==q2){
+                return true;
+            }
+            
+            int x=q1.front();
+            q1.pop();
+            q1.push(x);
+            i++;
+        }
         return false;
-    }
-    string t= s + s;
-    return t.find(goal) != string::npos;
+    
     }
 };
