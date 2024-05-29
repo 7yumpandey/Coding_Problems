eb4ff58952bc54212de5978c7e5fb0fe8e9b9518
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-05-30 02:09:28 +0600

    Time: 11 ms (37.74%), Space: 9.7 MB (33.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+    //     int n=s.size(),m=t.size();
+    //     if(n!=m){
+    //         return false;
+    //     }
+    //     if(s==t){
+    //         return true;
+    //     }
+    //     map<char,int>mp1,mp2;
+    //     for(int i=0;i<n;i++){
+    //         mp1[s[i]]++;
+    //         mp2[t[i]]++;
+    //     }
+    //     int a=mp1.size(),b=mp2.size();
+    //     if(a!=b){
+    //         return false;
+    //     }
+    //     for (auto x:mp1) {
+    //     char key =x.first;
+    //     if (mp2.find(key) == mp2.end() || mp1[key] != mp2[key]) {
+    //         return false;
+    //     }
+    // }
+    // return true;
+        
         int n=s.size(),m=t.size();
         if(n!=m){
             return false;
@@ -8,22 +32,17 @@ public:
         if(s==t){
             return true;
         }
-        map<char,int>mp1,mp2;
+        map<char,int>mp1;
         for(int i=0;i<n;i++){
             mp1[s[i]]++;
-            mp2[t[i]]++;
+            mp1[t[i]]--;
         }
-        int a=mp1.size(),b=mp2.size();
-        if(a!=b){
-            return false;
+        for(auto x:mp1){
+            if(x.second!=0){
+                return false;
+            }
         }
-        for (auto x:mp1) {
-        char key =x.first;
-        if (mp2.find(key) == mp2.end() || mp1[key] != mp2[key]) {
-            return false;
-        }
-    }
-    return true;
+        return true;
             
         
         
