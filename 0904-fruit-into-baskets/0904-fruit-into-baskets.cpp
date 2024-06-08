56619eee317315dbcd32b542f37f2ec8e02506ed
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-06-08 21:13:41 +0600

    Time: 119 ms (69.13%), Space: 74.8 MB (55.51%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -3,19 +3,27 @@ public:
     int totalFruit(vector<int>& fruits) {
         //longest array for having only 2 distinct fruit type;
         int left=0,right=0,ans=0;
-        unordered_map<int,int>mp;//to count distinct fruits
+        unordered_map<int,int>mp;
+        //to count distinct fruits
         while(right<fruits.size()){
             mp[fruits[right]]++;
+            if(mp.size()<=2){
+                ans=max(ans,right-left+1);
+            }
             if(mp.size()>2){
-                while(left<=right&&mp.size()>2){
-                    mp[fruits[left]]--;
+                // while(left<=right&&mp.size()>2){
+                //     mp[fruits[left]]--;
+                //     if(mp[fruits[left]]==0){
+                //         mp.erase(fruits[left]);
+                //     }
+                    if(mp[fruits[left]]>0){
+                        mp[fruits[left]]--;
+                    }
                     if(mp[fruits[left]]==0){
                         mp.erase(fruits[left]);
                     }
                     left++;
                 }
-            }
-                ans=max(ans,right-left+1);
             right++;
         }
         return ans;
