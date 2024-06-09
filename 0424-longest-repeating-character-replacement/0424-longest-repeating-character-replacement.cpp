87052edc76e7e8f77a23e8e2b2024b7a90bb3f22
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-06-09 18:46:27 +0600

    Time: 9 ms (54.06%), Space: 9.7 MB (7.18%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int left=0,right=0,maxfreq=0,ans=0;
+    
+        unordered_map<char,int>mp;
+        while(right<s.size()){
+            mp[s[right]]++;
+            maxfreq=max(maxfreq,mp[s[right]]);
+            int changes=(right-left)+1-maxfreq;
+            //right will tell the length of current subarray and the allowed changes can calculated jada wala char minus kar ke
+            if(changes<=k){
+                ans=max(ans,right-left+1);
+            }
+            if(changes>k){
+                if(mp[s[left]]>0){
+                    mp[s[left]]--;
+                }
+                
+                if(mp[s[left]]==0){
+                    mp.erase(s[left]);
+                }
+                left++;
+            }
+            
+            right++;
+        }
+        return ans;
+    }
+};
