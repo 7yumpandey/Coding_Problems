b18c306cf07d5f14f6e39fb1356a5c14ee10ac07
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-06-10 01:36:05 +0600

    Time: 24 ms (66.19%), Space: 11.2 MB (74.57%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -3,23 +3,27 @@ public:
     int maxConsecutiveAnswers(string s, int k) {
          int left=0,right=0,maxfreq=0,ans=0;
     
-        unordered_map<char,int>mp;
+        int t=0,f=0;
         while(right<s.size()){
-            mp[s[right]]++;
-            maxfreq=max(maxfreq,mp[s[right]]);
+            if(s[right]=='T'){
+                t++;
+            }
+            else{
+                f++;
+            }
+            maxfreq=max({maxfreq,t,f});
             int changes=(right-left)+1-maxfreq;
             //right will tell the length of current subarray and the allowed changes can calculated jada wala char minus kar ke
             if(changes<=k){
                 ans=max(ans,right-left+1);
             }
             if(changes>k){
-                if(mp[s[left]]>0){
-                    mp[s[left]]--;
-                }
-                
-                if(mp[s[left]]==0){
-                    mp.erase(s[left]);
-                }
+                if(s[left]=='T'){
+                t--;
+            }
+            else{
+                f--;
+            }
                 left++;
             }
             
