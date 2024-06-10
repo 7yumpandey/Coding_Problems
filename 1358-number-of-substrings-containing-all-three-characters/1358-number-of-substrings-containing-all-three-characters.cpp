36b178fb2a6d20833a0232dd841b82ef1d82c84c
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-06-10 18:26:56 +0600

    Time: 26 ms (35.73%), Space: 10.9 MB (27.10%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int left=0,right=0,ans=0,minfreq=INT_MAX;
-        unordered_map<char,int>mp;
-        while(right<s.size()){
-        mp[s[right]]++;
+//         int left=0,right=0,ans=0,minfreq=INT_MAX;
+//         unordered_map<char,int>mp;
+//         while(right<s.size()){
+//         mp[s[right]]++;
 
-        while (mp.size() == 3) {
-            ans += s.size() - right;
-            mp[s[left]]--;
-            if (mp[s[left]] == 0) {
-                mp.erase(s[left]);
-            }
-            left++;
-        }
+//         while (mp.size() == 3&&left<=right) {
+//             ans += s.size() - right;
+//             mp[s[left]]--;
+//             if (mp[s[left]] == 0) {
+//                 mp.erase(s[left]);
+//             }
+//             left++;
+//         }
         
-        right++;
+//         right++;
+//         }
+//         return ans;
+        
+        map<char,int>mp;int ans=0;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]=i;
+            if(mp.size()==3){
+                int mini=INT_MAX;
+                for(auto x:mp){
+                    if(x.second<mini){
+                        mini=x.second;
+                    }
+                }
+                ans+=mini+1;
+            }
         }
         return ans;
     }
