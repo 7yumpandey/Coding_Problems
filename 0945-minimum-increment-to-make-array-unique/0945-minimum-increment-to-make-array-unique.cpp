a2a0459e26559a42e4633784b291879bc2979e49
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-06-15 02:04:02 +0600

    Time: 124 ms (45.99%), Space: 69.1 MB (79.77%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -3,12 +3,13 @@ public:
     int minIncrementForUnique(vector<int>& nums) {
        sort(nums.begin(), nums.end());
         int ans = 0;
-        int nxt = nums[0] + 1;
+        int mini= nums[0] + 1;
         for(int i = 1; i < nums.size(); i++) {
-            if(nxt >= nums[i]) {
-                ans += (nxt++ - nums[i]);
+            if(mini >= nums[i]) {
+                ans += (mini - nums[i]);
+                mini++;
             } else {
-                nxt = nums[i] + 1;
+                mini = nums[i] + 1;
             }
         }
         return ans;
