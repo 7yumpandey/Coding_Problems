7677ed8c8a3a72c9d33db24a80394bc9c08d6c3a
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-08-05 01:39:46 +0530

    Time: 7 ms (35.87%), Space: 23.2 MB (22.89%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,57 +1,28 @@
 class Solution {
 public:
-
     string reverseWords(string s) {
-        // stack<string>st;
-        // string str="";
-        // for(int i=0;i<s.size();i++){
-        //     if(s[i]==' '){
-        //         if(str!=""){
-        //         st.push(str);
-        //         str="";
-        //         }
-        //     }
-        //     else{
-        //         str+=s[i];
-        //     }
-        // }
-        // if(str!=""){
-        //     st.push(str);
-        // }
-        // string ans="";
-        // while(!st.empty()){
-        //      string a=st.top();
-        //      st.pop();
-        //      ans += a;
-        //      if (!st.empty()) {
-        //          ans +=" ";
-        //      }
-        // }
-        // return ans;
-        
-        int n=s.size();
-        int left=0;
-        string ans="",temp="";
-        while(left<n){
-            char ch= s[left];
-        if (ch != ' ') {
-            temp += ch;
-        } else if (ch == ' ') {if (temp != "") {
-                if (ans != "") {
-                    ans = temp + " " + ans;
-                } else {
-                    ans = temp;
+        string word="", ans="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' '){
+                if(word!=""&&ans!=""){
+                ans=word+' '+ans;
                 }
-                temp = "";
+                else if(word!=""){
+                    ans=word;
+                }
+                word="";
+            }
+            else{
+                word+=s[i];
             }
         }
-        left++;
-        }
-        
-        if (temp!="") {
-        if (ans!=""&&temp!="") ans = temp + " " + ans;
-        else ans = temp;
-    }
+        if(word!=""&&ans!=""){
+                ans=word+' '+ans;
+                }
+                else if(word!=""){
+                    ans=word;
+                }
+
         return ans;
         
     }
