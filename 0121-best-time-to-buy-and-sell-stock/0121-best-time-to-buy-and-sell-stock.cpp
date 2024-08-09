146f21811c936b33d4d6d128fafeb9eadd9d4548
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-08-09 16:01:46 +0530

    Time: 89 ms (42.90%), Space: 96 MB (47.90%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans=0;
-        int n=prices.size();
-        int mindayind=0;
-        int maxdayind=0;
-        for(int i=1;i<n;i++){
-            if(prices[mindayind]>prices[i]){
-                mindayind=i;
-            }
-            if(maxdayind<mindayind){
-                maxdayind=mindayind;
-            }
-            if(prices[i]>prices[maxdayind]){
-                maxdayind=i;
-            }
-            ans=max(ans,prices[maxdayind]-prices[mindayind]);
+//         int ans=0;
+//         int n=prices.size();
+//         int mindayind=0;
+//         int maxdayind=0;
+//         for(int i=1;i<n;i++){
+//             if(prices[mindayind]>prices[i]){
+//                 mindayind=i;
+//             }
+//             if(maxdayind<mindayind){
+//                 maxdayind=mindayind;
+//             }
+//             if(prices[i]>prices[maxdayind]){
+//                 maxdayind=i;
+//             }
+//             ans=max(ans,prices[maxdayind]-prices[mindayind]);
             
+//         }
+//         return ans;
+        int ans=0,mini=prices[0];
+        for(int i=0;i<prices.size();i++){
+            int cost=prices[i]-mini;
+            ans=max(ans,cost);
+            mini=min(mini,prices[i]);
         }
         return ans;
     }
