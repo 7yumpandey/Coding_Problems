280b05fe55cb5498f94393b8c344356da5984b93
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-11-19 16:20:05 +0530

    Time: 0 ms (100.00%), Space: 19.7 MB (28.04%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int num=0;
-        for(int i=0;i<nums.size();i++){
-            num^=nums[i];
+        int ans=nums[0];
+        for(int i=1;i<nums.size();i++){
+            ans^=nums[i];
         }
-        return num;
-        
+        return ans;
     }
 };
