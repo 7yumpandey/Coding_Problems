54e09c2b8b4a1babc1f1e60a22037e70f5902716
Satyam Pandey <142084955+7yumpandey@users.noreply.github.com>
2024-12-15 03:10:07 +0530

    Time: 890 ms (7.32%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
 select name
 from Customer
-WHERE referee_id IS NULL OR referee_id <>2;
+# WHERE referee_id IS NULL OR referee_id <>2;
+WHERE COALESCE(referee_id,0) <>2;
+
